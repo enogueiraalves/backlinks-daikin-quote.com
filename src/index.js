d7ef709f8307c9802ca778d7671a68c061fb592f
enogueiraalves <orlando.jva@gmail.com>
2025-07-24 15:16:37 -0300

    Atualizações automáticas

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url)
-    const path = url.pathname
-
-    const redirects = {
-      "": "https://melhoradegaclimatizada.com.br/climatizacao-inteligente-adegas-e-ar-condicionado/",
-      "/": "https://melhoradegaclimatizada.com.br/climatizacao-inteligente-adegas-e-ar-condicionado/",
-      "/br/": "https://melhoradegaclimatizada.com.br/climatizacao-inteligente-adegas-e-ar-condicionado/",
-      "/br/#/login": "https://melhoradegaclimatizada.com.br/climatizacao-inteligente-adegas-e-ar-condicionado/"
-    }
-
-    if (redirects[path]) {
-      return Response.redirect(redirects[path], 301)
-    }
-
-    return new Response("Página não encontrada", { status: 404 })
+    const redirect = "https://melhoradegaclimatizada.com.br/climatizacao-inteligente-adegas-e-ar-condicionado/";
+    return Response.redirect(redirect, 301)
   }
-} 
+}
